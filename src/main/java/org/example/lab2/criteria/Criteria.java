d817e38798c7dc837b55561b05a791e7af83d574
literandltx <romanskovronn@gmail.com>
2024-12-10 21:27:57 +0200

    Add criteria 2.2

--- src/main/java/org/example/lab2/criteria/Criteria.java
+++ src/main/java/org/example/lab2/criteria/Criteria.java
@@ -17,6 +17,12 @@ public class Criteria {
     private static final int criteria21LetterKf = 2;
     private static final int criteria21BigramKf = 700;
 
+    private static final int criteria22LetterLimit = 50_000;
+    private static final int criteria22BigramLimit = 30;
+    private static final int criteria22LetterKf = 1;
+    private static final int criteria22BigramKf = 1;
+
+
     public static int criteriaZero(final List<String> texts, final String mainText, final int exp, final int FP) {
         int falseCounter = 0; // H1
 
@@ -95,8 +101,48 @@ public class Criteria {
         return FP == 1 ? texts.size() - falseCounter : falseCounter;
     }
 
-    public static int criteriaTwo(final List<String> texts, final String mainText, final int exp, final int FFPP) {
-        return 0;
+    public static int criteriaTwo(final List<String> texts, final String mainText, final int exp, final int FP) {
+        int falseCounter = 0;
+
+        if (exp == 1) {
+            final Map<Character, Integer> afrq = getPopularLetterFrequencies(mainText, criteria22LetterLimit);
+
+            for (final String text : texts) {
+                final Map<Character, Integer> tfrq = getPopularLetterFrequencies(mainText, criteria22LetterLimit);
+
+                for (int j = 0; j < texts.getFirst().length(); j++) {
+                    char temp = text.charAt(j);
+
+                    if (afrq.containsKey(temp)) {
+                        if (tfrq.get(temp) < criteria22LetterKf) {
+                            falseCounter++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (exp == 2) {
+            final Map<String, Integer> afrq = getPopularBigramFrequencies(mainText, criteria22LetterLimit);
+
+            for (final String text : texts) {
+                final Map<String, Integer> tfrq = getPopularBigramFrequencies(mainText, criteria22BigramLimit);
+
+                for (int j = 0; j++ < texts.getFirst().length() - 2; j += 2) {
+                    String temp = text.substring(j, j + 2);
+
+                    if (afrq.containsKey(temp)) {
+                        if (tfrq.get(temp) < criteria22BigramKf) {
+                            falseCounter++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return FP == 1 ? texts.size() - falseCounter : falseCounter;
     }
 
     public static int criteriaThree(final List<String> texts, final String mainText, final int exp, final int FFPP) {
